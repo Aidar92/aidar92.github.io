5ff5be34b925de4c2fa8d7d6851b75105215c7b9
Aidar92 <gumerovaidar@yandex.ru>
2020-01-25 13:51:49 +0300

    log user id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 document.getElementById('vk_auth_btn').addEventListener('click', () => {
     
-    VK.Auth.login(() => {
-
-    }, VK.access.FRIENDS)
+    VK.Auth.login(({session}) => {
+        console.log(session.user.id)
+    }, VK.access.FRIENDS | VK.access)
 })
