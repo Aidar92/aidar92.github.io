f656cf926876335908dcf6890fd2fbaacb4e264b
Aidar92 <gumerovaidar@yandex.ru>
2020-01-25 14:03:46 +0300

    log friends response

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ document.getElementById('vk_auth_btn').addEventListener('click', () => {
             user_ids: session.user.id,
             v: "5.73"
         }, r => {
+            console.log(r)
             if (r.response) {
                 document.getElementById('friends-list').innerHTML = `Количество друзей  ${r.response[0].count}`
             }
