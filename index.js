77f031b4d167a85359c27a96cf106d89bd157a82
Aidar92 <gumerovaidar@yandex.ru>
2020-01-25 12:51:58 +0300

    add onload method

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-document.getElementById('vk_auth_btn').addEventListener('click', () => {
+window.onload = () => {
     VK.init({
         apiId: 7294436
     })
+}
+
+document.getElementById('vk_auth_btn').addEventListener('click', () => {
+    
     VK.Auth.login(() => {
 
     }, VK.access.FRIENDS)
