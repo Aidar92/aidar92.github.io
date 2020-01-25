e748b2ddf5f3b44bbc687ed31cd1df55ad28d199
Aidar92 <gumerovaidar@yandex.ru>
2020-01-25 14:01:43 +0300

    get friends count

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ document.getElementById('vk_auth_btn').addEventListener('click', () => {
     
     VK.Auth.login(({session}) => {
         console.log(session.user.id)
+        VK.Api.call('friends.get', {
+            user_ids: session.user.id,
+            v: "5.73"
+        }, r => {
+            if (r.response) {
+                document.getElementById('friends-list').innerHTML = `Количество друзей  ${r.response[0].count}`
+            }
+        })
     }, VK.access.FRIENDS | VK.access)
 })
