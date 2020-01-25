69c2facb79c2670f95f9a2e67b3d442fd407abad
Aidar92 <gumerovaidar@yandex.ru>
2020-01-25 22:19:24 +0300

    add v param to request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const getProfileInfo = ({session}) => {
     VK.Api.call('users.get', {
         user_ids: session.user.id,
+        v: "5.73",
         fields: 'photo_50'
     }, r => { console.log(r); document.getElementById('avatar').src = r.response[0].photo_50 })
     VK.Api.call('friends.get', {
