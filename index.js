9ebd9e453f27aa881a6bba15640e261b8eaae53c
Aidar92 <gumerovaidar@yandex.ru>
2020-01-25 21:32:51 +0300

    add friends search

--- index.js
+++ index.js
@@ -3,19 +3,19 @@
 document.getElementById('vk_auth_btn').addEventListener('click', () => {
     
     VK.Auth.login(({session}) => {
-        console.log(session.user.id)
         VK.Api.call('friends.get', {
             user_ids: session.user.id,
             v: "5.73",
             fields: 'first_name,last_name'
         }, r => {
-            console.log(r)
             if (r.response) {
-                document.getElementById('friend-list').innerHTML = `
-                Количество друзей  ${r.response.count}
-                ${r.response.items.map(friend => `<li>${friend.first_name} ${friend.last_name}}`)}
+                document.getElementById('friend-count').innerHTML = `Количество друзей:  ${r.response.count}`
+                document.getElementById('friend-list').innerHTML = `                
+                    ${r.response.items.map(friend => `<li>${friend.first_name} ${friend.last_name}</li>`)}
                 `
-
+                document.getElementById('query').addEventListener('change', e => {
+                    document.getElementById('friend-list').innerHTML = r.response.items.filter(item => item.first_name.indexOf(e.target.value) || item.last_name.indexOf(e.target.value))
+                })
             }
         })
     }, VK.access.FRIENDS | VK.access)
