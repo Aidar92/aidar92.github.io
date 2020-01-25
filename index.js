66ac309fba904f64e20ebd3d57e475056d54f824
Aidar92 <gumerovaidar@yandex.ru>
2020-01-25 22:06:08 +0300

    add avatar

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-
-
-document.getElementById('vk_auth_btn').addEventListener('click', () => {
-    
-    VK.Auth.login(({session}) => {
-        VK.Api.call('friends.get', {
-            user_ids: session.user.id,
-            v: "5.73",
-            fields: 'first_name,last_name'
-        }, r => {
-            if (r.response) {
-                document.getElementById('friend-count').innerHTML = `Количество друзей:  ${r.response.count}`
-                document.getElementById('friend-list').innerHTML = `                
-                    ${r.response.items.map(friend => `<li>${friend.first_name} ${friend.last_name}</li>`)}
-                `
-                document.getElementById('query').addEventListener('input', e => {
-                    console.log(e.target.value)
-                    document.getElementById('friend-list').innerHTML = r.response.items
-                    .filter(item => item.first_name.toLowerCase().indexOf(e.target.value) !== -1 || item.last_name.toLowerCase().indexOf(e.target.value) !== -1)
-                    .map(friend => `<li>${friend.first_name} ${friend.last_name}</li>`)
-                })
-            }
-        })
-    }, VK.access.FRIENDS | VK.access)
+const getProfileInfo = ({session}) => {
+    VK.Api.call('users.get', {
+        user_ids: session.user.id,
+        fields: 'photo_50'
+    }, r => document.getElementById('avatar').src = r.response[0].photo_50)
+    VK.Api.call('friends.get', {
+        user_ids: session.user.id,
+        v: "5.73",
+        fields: 'first_name,last_name'
+    }, r => {
+        if (r.response) {
+            document.getElementById('friend-count').innerHTML = `Количество друзей:  ${r.response.count}`
+            document.getElementById('friend-list').innerHTML = `                
+                ${r.response.items.map(friend => `<li>${friend.first_name} ${friend.last_name}</li>`)}
+            `
+            document.getElementById('query').addEventListener('input', e => {
+                document.getElementById('friend-list').innerHTML = r.response.items
+                .filter(item => item.first_name.toLowerCase().indexOf(e.target.value) !== -1 || item.last_name.toLowerCase().indexOf(e.target.value) !== -1)
+                .map(friend => `<li>${friend.first_name} ${friend.last_name}</li>`)
+            })
+        }
+    })
+}
+window.onload = () => {
+    VK.Auth.getLoginStatus(response => {
+        if (response.session) getProfileInfo(response)
+    })
+}
+document.getElementById('vk_auth_btn').addEventListener('click', () => {    
+    VK.Auth.login(getProfileInfo, VK.access.FRIENDS | VK.access)
 })
+
