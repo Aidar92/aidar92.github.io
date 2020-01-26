07361f55b601d25bb2986358a9e19186fd331311
Aidar92 <gumerovaidar@yandex.ru>
2020-01-26 13:08:02 +0300

    add styles

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const getProfileInfo = ({ session }) => {
         VK.Api.call('users.get', {
             user_ids: session.mid,
             v: "5.73",
-            fields: 'photo_50'
-        }, r => { document.getElementById('avatar').src = r.response[0].photo_50 })
+            fields: 'photo_100'
+        }, r => { document.getElementById('avatar').src = r.response[0].photo_100 })
         VK.Api.call('friends.get', {
             user_ids: session.mid,
             v: "5.73",
             fields: 'first_name,last_name'
         }, r => {
             if (r.response) {
-                document.getElementById('friend-count').innerHTML = `Количество друзей:  ${r.response.count}`
-                document.getElementById('friend-list').innerHTML = `                
-                ${r.response.items.map(friend => `<li>${friend.first_name} ${friend.last_name}</li>`)}
+                document.getElementById('friends-count').innerHTML = `Количество друзей:  ${r.response.count}`
+                document.getElementById('friends-list').innerHTML = `                
+                ${r.response.items.map(friend => `<li class="friends-list-item">${friend.first_name} ${friend.last_name}</li>`).join("")}
             `
                 document.getElementById('query').addEventListener('input', e => {
-                    document.getElementById('friend-list').innerHTML = r.response.items
+                    document.getElementById('friends-list').innerHTML = r.response.items
                         .filter(item => item.first_name.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1 || item.last_name.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1)
-                        .map(friend => `<li>${friend.first_name} ${friend.last_name}</li>`)
+                        .map(friend => `<li class="friends-list-item">${friend.first_name} ${friend.last_name}</li>`).join("")
                 })
             }
         })
